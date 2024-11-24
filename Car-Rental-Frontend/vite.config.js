41ded7e868b1818c65b3b7229dad17ff4a8d95e6
VGayu36 <adhigayu36@gmail.com>
2024-11-24 19:44:38 +0800

    Fix: Updated Vite config to set publish directory

--- Car-Rental-Frontend/vite.config.js
+++ Car-Rental-Frontend/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'), // Use @ for shorter imports
     },
   },
   build: {
-    outDir: 'publish', // Specify your output directory for build
-  }
-})
+    outDir: 'publish', // Output directory for the build files
+  },
+});
